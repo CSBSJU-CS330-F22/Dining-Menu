8f096ef3587551bf6a9e85f97000124fed698823
JOLSEN001 <jolsen001@vplincsci056.ad.csbsju.edu>
2022-10-12 23:26:46 -0500

    Added review and DBController classes

---
+++ src/main/java/interaction/DBController.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package interaction;
+import java.util.*;
+
+/**
+ * @author jolsen001
+ *
+ */
+public class DBController {
+
+	public DBController(){
+		
+	}
+	
+	public ArrayList<String> getFood(String name){
+		return null;
+	}
+	
+	public ArrayList<String> getUser(String username){
+		return null;
+	}
+	
+	public String getMenuItem(String date, String mealType) {
+		return null;
+	}
+	
+	public ArrayList<String> getFoodReview(String email, String foodItem){
+		return null;
+	}
+	
+	
+}
+
