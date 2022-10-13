5b36a7a7cc97d327b8280ee1f0c45e0f8923c102
Jayden Brownlee <Jbrownlee001@csbsju.edu>
2022-10-13 10:36:19 -0500

    Working on tests

--- tests/Food/FoodTest.java
+++ tests/Food/FoodTest.java
@@ -1,15 +1,22 @@
 package Food;
 
-import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class FoodTest {
+public class FoodTest extends TestCase{
+	Food fd = new Food();
 
 	@Before
 	public void setUp() throws Exception {
+		fd.setName("Cookie");
+		fd.setCalories("60");
+		fd.setFat("6");
+		fd.setCarbs("5");
+		fd.setProtein("3");
+		fd.setCholesterol("4");
 	}
 
 	@After
@@ -17,8 +24,33 @@ public class FoodTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSetName() {
+		assertEquals("Cookie", fd.getName());
+	}
+	
+	@Test
+	public void testSetCalories() {
+		assertEquals("60", fd.getCalories());
+	}
+	
+	@Test
+	public void testSetFat() {
+		assertEquals("6", fd.getFat());
+	}
+	
+	@Test
+	public void testSetCarbs() {
+		assertEquals("5", fd.getCarbs());
+	}
+	
+	@Test
+	public void testSetProtein() {
+		assertEquals("3", fd.getProtein());
+	}
+	
+	@Test
+	public void testSetsetCholesterol() {
+		assertEquals("4", fd.getCholesterol());
 	}
 
 }
