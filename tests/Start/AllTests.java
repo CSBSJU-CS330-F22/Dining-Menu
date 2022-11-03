24e7a30e309d11231f84522f63d19b9cc14aeb00
Jayden Brownlee <Jbrownlee001@csbsju.edu>
2022-11-03 14:59:38 -0500

    test classes

--- tests/Start/AllTests.java
+++ tests/Start/AllTests.java
@@ -5,6 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import Food.FoodTest;
+import Food.ReviewTest;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
@@ -15,6 +16,7 @@ public class AllTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite(AllTests.class.getName());
 		suite.addTestSuite(FoodTest.class);
+		suite.addTestSuite(ReviewTest.class);
 		return suite();
 	}
 
