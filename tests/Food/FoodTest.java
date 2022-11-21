77a66fc21652973038bf117c71bd723e13a42d8f
Jayden Brownlee <Jbrownlee001@csbsju.edu>
2022-11-21 11:02:07 -0600

    added documentation to testing Not Complete

--- tests/Food/FoodTest.java
+++ tests/Food/FoodTest.java
@@ -8,12 +8,17 @@ import java.util.ArrayList;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+/**
+ * @author Jayden Brownlee
+ */
 
 public class FoodTest extends TestCase{
 	
 	Food fd;
 	
-
+/*
+ * Sets up the initial array list and passes the list into the constructor
+ */
 	@Before
 	public void setUp() throws Exception {
 		ArrayList<String> test = new ArrayList<String>();
@@ -30,67 +35,103 @@ public class FoodTest extends TestCase{
 	public void tearDown() throws Exception {
 	}
 
+	/*
+	 * Sets the name and tests to make sure that the name matches the one set
+	 */
 	@Test
 	public void testSetName() {
 		fd.setName("Chips");
 		assertEquals("Chips", fd.getName());
 	}
 	
+	/*
+	 * Sets the calories and tests to make sure that the name matches the one set
+	 */
 	@Test
 	public void testSetCalories() {
 		fd.setCalories("30");
 		assertEquals("30", fd.getCalories());
 	}
 	
+	/*
+	 * Sets the fat and tests to make sure that the name matches the one set
+	 */
 	@Test
 	public void testSetFat() {
 		fd.setFat("4");
 		assertEquals("4", fd.getFat());
 	}
 	
+	/*
+	 * Sets the carbs and tests to make sure that the name matches the one set
+	 */
 	@Test
 	public void testSetCarbs() {
 		fd.setCarbs("20");
 		assertEquals("20", fd.getCarbs());
 	}
 	
+	/*
+	 * Sets the protein and tests to make sure that the name matches the one set
+	 */
 	@Test
 	public void testSetProtein() {
 		fd.setProtein("5");
 		assertEquals("5", fd.getProtein());
 	}
 	
+	/*
+	 * Sets the Cholesterol and tests to make sure that the name matches the one set
+	 */
 	@Test
 	public void testSetCholesterol() {
 		fd.setCholesterol("6");
 		assertEquals("6", fd.getCholesterol());
 	}
 	
+	/*
+	 * Grabs the Cholesterol and tests to see if it matches from the constructor
+	 */
 	@Test
 	public void testGetCholesterol() {
 		assertEquals("4", fd.getCholesterol());
 	}
 	
+	/*
+	 * Grabs the Protein and tests to see if it matches from the constructor
+	 */
 	@Test
 	public void testGetProtein() {
 		assertEquals("3", fd.getProtein());
 	}
 	
+	/*
+	 * Grabs the Carbs and tests to see if it matches from the constructor
+	 */
 	@Test
 	public void testGetCarbs() {
 		assertEquals("5", fd.getCarbs());
 	}
 	
+	/*
+	 * Grabs the Fat and tests to see if it matches from the constructor
+	 */
 	@Test
 	public void testGetFat() {
 		assertEquals("6", fd.getFat());
 	}
 	
+	/*
+	 * Grabs the Calories and tests to see if it matches from the constructor
+	 */
 	@Test
 	public void testGetCalories() {
 		assertEquals("60", fd.getCalories());
 	}
 	
+	/*
+	 * Grabs the Name and tests to see if it matches from the constructor
+	 */
 	@Test
 	public void testGetName() {
 		assertEquals("Cookie", fd.getName());
--- tests/Menu/MenuTest.java
+++ tests/Menu/MenuTest.java
@@ -10,12 +10,19 @@ import org.junit.jupiter.api.Test;
 
 import Food.Food;
 
+/**
+ * @author Jayden Brownlee
+ */
 class MenuTest {
 	Menu mn;
 	Food item;
 	Food item2;
 	
-	
+	/*
+	 * Test: list filled with weekly informations
+	 * Food: a initial list to pass through Menu Test
+	 * Dish: Food Item to pass through Menu test
+	 */
 	@BeforeEach
 	void setUp() throws Exception {
 		ArrayList<String> test = new ArrayList<String>();
@@ -46,32 +53,51 @@ class MenuTest {
 	void tearDown() throws Exception {
 	}
 
+	/*
+	 * Grabs the date passed through and checks to make sure it matches with the list
+	 */
 	@Test
 	public void testGetDate() {
 		assertEquals("Monday", mn.getDate());
 	}
 	
+	/*
+	 * Sets the date and checks that it matches the one listed
+	 */
 	@Test
 	public void testSetDate() {
 		mn.setDate("Thursday");
 		assertEquals("Thursday", mn.getDate());
 	}
 	
+	/*
+	 * Grabs the Meal Type passed through and checks to make sure it matches with the list
+	 */
 	@Test
 	public void testGetMealType() {
 		assertEquals("Breakfast", mn.getMealType());
 	}
 	
+	/*
+	 * Sets the Meal Type and checks that it matches the one listed
+	 */
 	@Test
 	public void testSetMealType() {
 		mn.setMealType("Dinner");
 		assertEquals("Dinner", mn.getMealType());
 	}
 	
+	/*
+	 * Grabs the Food Item passed through and checks to make sure it matches with the list
+	 */
 	@Test
 	public void testGetFood() {
 		assertEquals(item, mn.getFood());
 	}
+	
+	/*
+	 * Sets the Food Item and checks that it matches the one listed
+	 */
 	@Test
 	public void testSetFoodItem() {
 		mn.setFood(item2);
